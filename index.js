0418892426849ca1954fd20ba96151f7bcb18d26
SoniGautam222 <gautamjisoni222@gmail.com>
2023-04-12 19:09:31 +0530

    added the router and the controller to the profile section and add the ejs

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express= require('express');
 const app=express();
 const port = 8000;
-
-
-
 // using express router
 app.use('/',require('./routes'));
 
+
+app.set('view engine', "ejs");
+app.set('views','./views')
 
 app.listen(port,function(err){
     if(err){
