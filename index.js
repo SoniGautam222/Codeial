332656d0fcf49e2405ed5be21004ca21c3834c05
SoniGautam222 <gautamjisoni222@gmail.com>
2023-04-12 18:35:15 +0530

    add a controller and a router for the home

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app=express();
 const port = 8000;
 
 
+
+// using express router
+app.use('/',require('./routes'));
+
+
 app.listen(port,function(err){
     if(err){
         // console.log('Error : ', err);
