2bf202c85bf270357fc73a4759d1440f9c984446
SoniGautam222 <gautamjisoni222@gmail.com>
2023-04-20 17:31:20 +0530

    added the user sign in & sign up

--- controllers/users_controllers.js
+++ controllers/users_controllers.js
@@ -4,3 +4,28 @@ module.exports.profile = function(req,res){
         title: "profile",
     })
 }
+
+// render the sign up page
+module.exports.signUp=function(req,res){
+    return res.render('user_sign_up',{
+        title : "Codeial | Sign Up"
+    })
+}
+
+// render the sign in page
+module.exports.signIn=function(req,res){
+    return res.render('user_sign_in',{
+        title : "Codeial | Sign In"
+    })
+
+}
+
+// gett the sign up data
+module.exports.create = function(req,res){
+
+}
+
+// sign in and create a session for the user
+module.exports.createSession=function (req,res){
+
+}
