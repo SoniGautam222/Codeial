e90eaea6428499d80895e6e9923a1fdc42c97670
SoniGautam222 <gautamjisoni222@gmail.com>
2023-05-04 21:29:47 +0530

    added the mongo store

--- config/passport-local-stratergy.js
+++ config/passport-local-stratergy.js
@@ -11,17 +11,18 @@ passport.use(new LocalStrategy({
     },
     function(email,password,done){
         // find a user and establish the identity
-        User.findOne({email:email}, function(err,user){
-            if(err){
-                console.log('error in finding user --> passport');
-                return done(err);
-            }
+        User.findOne({email:email})
+           .then(user=>{
             if(!user || user.password !=password){
                 console.log('invalid user name and the password');
                 return done(null, false);
             }
             return done(null, user);
         })
+        .catch(err=>{
+                console.log('error in finding user --> passport');
+                return done(err);
+        });
     }
     
 ));
@@ -34,13 +35,32 @@ passport.serializeUser(function(user,done){
 
 // deserializing the user from the key in the cookies..
 passport.deserializeUser(function(id,done){
-    User.findById(id,function(err,user){
-        if(err){
-            console.log('error in finding user --> passport');
-                return done(err);
-        }
-        return done(null,done);
+    User.findById(id)
+    .then(function(user){
+        done(null,user);
+    })
+    .catch(function(err){
+        done(err);
     });
 });
 
+// check if the user is authenticated
+passport.checkAuthentication = function(req,res,next){
+    if (req.isAuthenticated()){
+        return next();
+    }
+
+    // if the user is not signed in
+    return res.redirect('/users/sign-in');
+}
+
+passport.setAuthenticatedUser = function(req,res,next){
+    if (req.isAuthenticated()){
+        // req.user contains the current signed in user from the cookie and we are just sending this to the locals 
+        // for the views
+        res.locals.user =req.user;
+    }
+    next();
+}
+    
 module.exports=passport;
--- controllers/users_controllers.js
+++ controllers/users_controllers.js
@@ -1,3 +1,5 @@
+const User =require('../models/user');
+
 module.exports.profile = function(req,res){
     // res.end('<h1>User Profile</h1');
     return res.render('user_profile',{
@@ -7,6 +9,11 @@ module.exports.profile = function(req,res){
 
 // render the sign up page
 module.exports.signUp=function(req,res){
+    if(req.isAuthenticated()){
+       return res.redirect('/users/profile');
+    }
+
+
     return res.render('user_sign_up',{
         title : "Codeial | Sign Up"
     })
@@ -14,18 +21,33 @@ module.exports.signUp=function(req,res){
 
 // render the sign in page
 module.exports.signIn=function(req,res){
+    if(req.isAuthenticated()){
+       return res.redirect('/users/profile');
+    }
     return res.render('user_sign_in',{
         title : "Codeial | Sign In"
     })
 
 }
 
-// gett the sign up data
-module.exports.create = function(req,res){
+// get the sign up data
 
-}
+module.exports.create = async function (req, res) {
+    if (req.body.password != req.body.comfirm_password) {
+        console.log(' passport are not correct !!');
+      return res.redirect("back");
+    }
+    let user = await User.findOne({ email: req.body.email });
+    if (!user) {
+        console.log('increate !!');
+      await User.create(req.body);
+      return res.redirect("/users/sign-in");
+    }
+    console.log('user already created');
+    return res.redirect("back");
+  };
 
 // sign in and create a session for the user
 module.exports.createSession=function (req,res){
-
+    return res.redirect('/');
 }
