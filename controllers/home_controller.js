77e0565b1547565811275bc312e494238b56e361
SoniGautam222 <gautamjisoni222@gmail.com>
2023-05-05 11:47:50 +0530

    show the user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,18 @@
+const Post = require('../models/post');
+
 module.exports.home= function(req,res){
-  console.log(req.cookies);
-  res.cookie('user_id',25)
-  return res.render('home',{
-    title : "Home"
-  })
-}
+  // console.log(req.cookies);
+  // res.cookie('user_id',25)
+  Post.find({})
+  .populate('user')
+    .then(posts => {
+      res.render('home', {
+        title: 'Codeial: Home',
+        posts: posts
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      return res.redirect('back');
+    });
+  }
