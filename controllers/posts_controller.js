61ee95c39cfaff081ca21f979fbc392af7c54636
SoniGautam222 <gautamjisoni222@gmail.com>
2023-05-07 17:41:18 +0530

    add the deleting of the post

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,16 +1,33 @@
-const Post =require('../models/post');
+const Post = require('../models/post');
+const Comment = require('../models/comments');
 
+module.exports.create = async function(req, res) {
+  try {
+    let post = await Post.create({
+      content: req.body.content,
+      user: req.user._id
+    });
 
-module.exports.create = async function(req, res){
-    try {
-      let post = await Post.create({
-        content: req.body.content,
-        user: req.user._id
-      });
-  
+    return res.redirect('back');
+  } catch (error) {
+    console.error(error);
+    return res.redirect('back');
+  }
+}
+
+module.exports.destroy = async function(req, res) {
+  try {
+    let post = await Post.findByIdAndRemove(req.params.id);
+
+    if (post.user == req.user.id) {
+      await Comment.deleteMany({ post: req.params.id });
+
       return res.redirect('back');
-    } catch (error) {
-      console.error(error);
+    } else {
       return res.redirect('back');
     }
+  } catch (error) {
+    console.error(error);
+    return res.redirect('back');
   }
+}
